05aadc0d29eb1c949853cdcf4cdc5b925d7949cc
Biso Shrestha <biso@shrestha.ca>
2022-01-19 09:09:55 -0500

    Starting to add posts, turn the provided pages into templates, make fixes for use with Jekyll.

---
+++ _posts/2022-01-18-installation-and-initialization.md
@@ -0,0 +1,54 @@
+---
+layout: post
+title:  "Installation and Initialization"
+date:   2022-01-18 14:33:57 -0500
+categories: iOS
+---
+# Installation and Initialization
+
+The Judo SDK allows you to present Experiences in your iOS mobile app. It supports using push-driven automatic sync in order to ensure that Experiences are available instantly (and when offline).
+
+While the SDK may be linked with apps that have a target (minimum iOS SDK API level) of iOS 11, Judo content can only be displayed on iOS 13 and greater.
+
+You need your app's Judo Access Token (found at the [Judo web interface](https://www.judo.app/login)).  You will need to configure (if your team has not done so already) an iOS app in the Judo settings. Once you have done so, there you will find the app's Access Token.
+
+You will also a Domain configured in the Judo settings as well. These domains are subdomains within the `.judo.app` top-level domain.
+
+## Install the SDK
+
+The recommended way to install the Judo SDK is with SwiftPM.
+
+To add the dependency to your project using Xcode (12 or later assumed), choose File -> Swift Packages -> Add Package Dependency, and type the URL to this repository into the search field:
+
+    https://github.com/judoapp/judo-ios
+
+In this next prompt, you will be able to choose the default SwiftPM update policy. The Judo SDK uses the standard semver versioning strategy, so the default of "up to next major" is recommended.  Leave the settings at their defaults and press Next.
+
+It will prompt you to select which Judo products to add to your app target.  Make sure you select both JudoModel and JudoSDK, and press Next.
+
+Once you have completed the SwiftPM installation flow, the SDK has been installed, and what remains now to is to complete configuring and integrating it.
+
+## Initialize the SDK
+
+For the next steps you will need your Access Token (discussed above) and an App Domain. Note that Judo supports multiple domains.
+
+In your AppDelegate's `didFinishLaunchingWithOptions` template method, add the following lines (note you will need `import JudoSDK` to import the SDK's main module):
+
+```swift
+// Initialize the Judo SDK itself.
+Judo.initialize(accessToken: "<ACCESS TOKEN HERE>", domain: "<APP-DOMAIN-HERE>")
+// Request that Judo perform a sync on start. This is asynchronous, and in the event of no pending updates will consist of only a small, single HTTP request.
+```
+
+## Troubleshooting
+
+If the `JUDO_VERBOSE` environment variable is set to 1 (you can do so in your Target's Scheme in Xcode), the Judo SDK will log additional information about its behaviour.
+
+## Getting Help
+
+See the [Judo Forum](https://forum.judo.app/c/sdk-integration).
+
+---
+
+Continue to [Deep and Universal Links](Deep-and-Universal-Links).
+
---
+++ assets/js/scripts.js
@@ -0,0 +1,115 @@
+var retrievedData = localStorage.getItem("authedUser");
+var profileData = JSON.parse(retrievedData);
+//Show/hide logged in username, if user is logged in
+if (profileData) {
+  $('.account-nav-trigger').css('display', 'flex');
+  $('.site-header .btn').hide();
+  $('.mobile-nav-btn').hide();
+  var profileName = profileData.firstName + " " + profileData.lastName;
+  var profileEmail = profileData.email;
+  var firstNameLetter = profileData.firstName.charAt(0);
+  var lastNameLetter = profileData.lastName.charAt(0);
+  $('.account-name').text(profileName);
+  $('.account-email').text(profileEmail);
+  $('.first-name-letter').text(firstNameLetter);
+  $('.last-name-letter').text(lastNameLetter);
+}
+function handleLoggedInState() {
+  if (window.matchMedia("(max-width:991px)").matches) {
+    if (profileData) {
+    } else {
+      $('.mobile-nav-btn').css('display', 'inline-block');
+      $('.btn-nav').css('display', 'none');
+    }
+  } else {
+    if (profileData) {
+      $('.account-nav-trigger').css('display', 'flex');
+      $('.mobile-nav-btn').css('display', 'none');
+    } else {
+      $('.btn-nav').css('display', 'inline-block');
+    }
+  }
+}
+function checkScroll() {
+  if (window.matchMedia("(max-width:991px)").matches) {
+    $('.site-header').addClass('is-fixed');
+  } else {
+    $('.site-header').removeClass('is-fixed');
+    if ($(window).scrollTop() > 80){
+      $('.all-cats').addClass('is-fixed');
+    } else {
+      $('.all-cats').removeClass('is-fixed');
+    }
+  }
+}
+$( document ).ready(function() {
+  //Show/hide logged in menu
+  handleLoggedInState();
+  checkScroll();
+  //main menu
+  var open = $('.navbar-toggler');
+  open.click(function() {
+    var overlay = $('.overlay');
+    var nav = $('.all-cats');
+    var icon = $('.menu-toggle');
+    var topNav = $('.top-nav');
+    overlay.fadeToggle('fast');
+    nav.toggleClass("menu-open");
+    topNav.toggleClass("white");
+    icon.toggleClass('open');
+    $(".all-cats li").each(function(index){
+      $(this).css({
+        'animation-delay' : 0.1*(1+index) + 's'
+      });
+    });
+  });
+  //show/hide profile dropdown
+  $('.account-nav-trigger').click(function() {
+    $('.account-nav').fadeToggle(200);
+    $(this).find('.account-chevron').toggleClass('up');
+  });
+  //wrap iframes inside a responsive container
+  $('.doc-text iframe').wrap('<div class="video-container"></div>');
+  $('.video-container').wrap('<div class="iframe-container"></div>');
+  //expand child categories in the left sidebar
+  $('.parent-cat').click(function() {
+    $(this).next('.children-cats').slideToggle(300);
+    $(this).toggleClass('is-expanded');
+  });
+  //add chevron and show children articles in the left sidebar on an active article
+  $('.is-active').closest('.children-cats').css('display', 'block');
+  $('.is-active').closest('.children-cats').prev('.parent-cat').addClass('is-expanded');
+  //hides/shows left sidebar on mobile
+  $('.nav-title').click(function() {
+    $(this).next('.nav-container').slideToggle(300);
+    $(this).toggleClass('is-expanded');
+  });
+  linkScroll = $('.doc-nav a');
+  linkScroll.click(function(e){
+    e.preventDefault();
+    $('.doc-nav a').removeClass('is-active');
+    $(this).toggleClass('is-active');
+    $('body, html').animate({
+      scrollTop: $(this.hash).offset().top - 80
+    }, 500);
+  });
+});
+function addActiveToToc() {
+  var scrollTop = $(document).scrollTop();
+  var anchors = $('.doc-text section');
+  for (var i = 0; i < anchors.length; i++){
+    if (scrollTop > $(anchors[i]).offset().top - 85 && scrollTop < $(anchors[i]).offset().top + $(anchors[i]).height() - 85) {
+      $('.doc-nav a[href="#' + $(anchors[i]).attr('id') + '"]').addClass('is-active');
+    } else {
+      $('.doc-nav a[href="#' + $(anchors[i]).attr('id') + '"]').removeClass('is-active');
+    }
+  }
+}
+$(window).scroll(function() {
+  addActiveToToc();
+  checkScroll();
+});
+$(window).resize(function(){
+  handleLoggedInState();
+  checkScroll();
+});
